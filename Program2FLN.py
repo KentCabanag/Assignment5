dfca1013fb36b4970b87e166d364639241eceed6
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-14 01:45:36 +0800

    commit

---
+++ Program2FLN.py
@@ -0,0 +1,10 @@
+#Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
+#Example:
+#Input grade: 87.6
+#Grade/Mark: 1.75
+#Description: Very Good
+
+#Program 2: Find the lowest number
+#Create a program that ask 3 numbers. 
+#Find the lowest number using only if-else statement.
+#Display the lowest number
