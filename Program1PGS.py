d9de10fd693027a761edb627412aec9f9347c234
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-14 17:02:09 +0800

    commit

--- Program1PGS.py
+++ Program1PGS.py
@@ -12,49 +12,51 @@
 # 5.0           65-74           Failure
 # Inc.                          Incomplete
 # W                             Withdrawn
-# D                             Dropeed
+# D                             Dropee
+print("The PUP Student Handbook 2014: Section 8. Grading System")
+print()
 grade = round(float(input("Enter your Grade: ")))
 
 if grade > 100:
     print("There is no such grade that exceeds the maximum grade of 100.")
 
-elif grade >=97 and grade <=100:
+elif grade >= 97 and grade <= 100:
     print("Grade/Mark: 1.0")
     print("Description: Excellent")
 
-elif grade >=94 and grade <=96:
+elif grade >= 94 and grade <= 96:
     print("Grade/Mark: 1.25")
     print("Description: Excellent")
 
-elif grade >=91 and grade <=93:
+elif grade >= 91 and grade <= 93:
     print("Grade/Mark: 1.5")
     print("Description: Very Good")
 
-elif grade >=88 and grade <=90:
+elif grade >= 88 and grade <= 90:
     print("Grade/Mark: 1.75")
     print("Description: Very Good")
 
-elif grade >=85 and grade <=87:
+elif grade >= 85 and grade <= 87:
     print("Grade/Mark: 2.0")
     print("Description: Good")
 
-elif grade >=82 and grade <=84:
+elif grade >= 82 and grade <= 84:
     print("Grade/Mark: 2.25")
     print("Description: Good")
 
-elif grade >=79 and grade <=81:
+elif grade >= 79 and grade <= 81:
     print("Grade/Mark: 2.5")
     print("Description: Satisfactory")
 
-elif grade >=76 and grade <=78:
+elif grade >= 76 and grade <= 78:
     print("Grade/Mark: 2.75")
     print("Description: Satisfactory")
 
-elif grade ==75:
+elif grade == 75:
     print("Grade/Mark: 3.00")
     print("Description: Passing")
 
-elif grade >=65 and grade <=74:
+elif grade >= 65 and grade <= 74:
     print("Grade/Mark: 5.0")
     print("Description: Failure")
 
--- Program3LF.py
+++ Program3LF.py
@@ -1,3 +1,14 @@
+print("\n\033[31;1m The Life Stages of a Person\033[0m")
+print()
 Age = int(input("Enter your age: "))
-if Age >-1 and Age <= 12:
-    print("Kid")
+if Age > -1 and Age <= 12:
+    print("\n\033[34;1mKid\033[0m\033[33;1m")
+
+elif Age >= 13 and Age <= 17:
+    print("\n\033[34;1mTeen\033[0m\033[33;1m")
+
+elif Age == 18:
+    print("\n\033[34;1mDebut\033[0m\033[33;1m")
+
+else:
+    print("\n\033[34;1mAdult\033[0m\033[33;1m")
