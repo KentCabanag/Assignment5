27d3c875e978c10d5932908d9dfe32296e8be2be
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-11-17 02:53:30 +0800

    commit

--- Program2FLN.py
+++ Program2FLN.py
@@ -1,10 +1,41 @@
-#Create a program that will ask for grade percentage. Display the equivalent Grade/Mark and Description
-#Example:
-#Input grade: 87.6
-#Grade/Mark: 1.75
-#Description: Very Good
-
 #Program 2: Find the lowest number
 #Create a program that ask 3 numbers. 
 #Find the lowest number using only if-else statement.
 #Display the lowest number
+
+from typing import is_typeddict
+
+
+print("\n\033[33;3m Finding the Lowest Number\033[0m")
+print()
+def Lowest_number():
+    Fnum = int(input("Enter the First Number: "))
+    Snum = int(input("Enter the Second Number: "))
+    Tnum = int(input("Enter the Third Number: "))
+    print()
+
+    if Fnum < Snum and Fnum < Tnum:
+        print(f"The Lowest Number is {Fnum}.")
+    
+    elif Snum < Fnum and Snum < Tnum:
+        print(f"The Lowest Number is {Snum}.")
+    
+    elif Tnum < Fnum and Tnum < Snum:
+        print(f"The Lowest Number is {Tnum}.")
+
+    elif Fnum < Snum or Fnum < Tnum:
+        print(f"The Lowest Number is {Fnum}.")
+    
+    elif Snum < Fnum or Snum < Tnum:
+        print(f"The Lowest Number is {Snum}.")
+    
+    else:
+        Tnum < Fnum or Tnum < Snum
+        print(f"The Lowest Number is {Tnum}.")
+
+Lowest_number()
+
+
+
+
+
--- Program3LF.py
+++ Program3LF.py
@@ -1,14 +1,14 @@
-print("\n\033[31;1m The Life Stages of a Person\033[0m")
+print("\n\033[31;4mThe Life Stages of a Person\033[0m")
 print()
 Age = int(input("Enter your age: "))
 if Age > -1 and Age <= 12:
-    print("\n\033[34;1mKid\033[0m\033[33;1m")
+    print("\n\033[34;1m'Kid'\033[0m")
 
 elif Age >= 13 and Age <= 17:
-    print("\n\033[34;1mTeen\033[0m\033[33;1m")
+    print("\n\033[34;1m'Teen'\033[0m")
 
 elif Age == 18:
-    print("\n\033[34;1mDebut\033[0m\033[33;1m")
+    print("\n\033[34;1m'Debut'\033[0m")
 
 else:
-    print("\n\033[34;1mAdult\033[0m\033[33;1m")
+    print("\n\033[34;1m'Adult'\033[0m")
